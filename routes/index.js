953f13ae8279b7b18a34e4ff5fb47b9bac8d4e16
Chad McKee <chadm_@yahoo.com>
2017-06-22 09:48:51 -0400

    finished queries and rendering

--- routes/index.js
+++ routes/index.js
@@ -101,7 +101,7 @@ router.get('/product_info', function(req, res, next) {
         productInfo = data;
         var productCode = productInfo.productCode;
         return getOrderInfo(productCode);
-        
+
     }).then((data)=>{
         var orderInfo = data;
         var totalOrdered = 0;
@@ -113,27 +113,119 @@ router.get('/product_info', function(req, res, next) {
 
     });
 
+});
+
+
+router.get('/orders', function(req, res, next) {
+
+    var orderDetails = {orderNumber: req.query.msg, orderStatus: null, customerNumber: null,
+    customerName: null, customerCity: null, employeeNumber: null, employeeFirstName: null,
+    employeeLastName: null, employeeOffice: null, employeeReportsTo: null, productNames: [],
+    totalPrices: [], productCodes:[]};
+
+    function getOrderInfo() {
+        // console.log(req.query.msg);
+        orderNumber = req.query.msg;
+        // var idQuery = `SELECT id FROM player_info WHERE (first_name = '${nameArray[0]}' AND last_name = '${nameArray[1]}');`;
+        var orderInfoQuery = `SELECT status, customerNumber FROM orders WHERE orderNumber = '${orderNumber}';`;
+        return new Promise((resolve, reject) => {
+            // send off the AJAX request
+            connection.query(orderInfoQuery, function (error, results) {
+                if (error) throw error;
+                resolve(results[0]);
+            });
+        });
+    }
+
+    function getCustomerInfo(customerID){
+        var customerQuery = `SELECT customerName, city, salesRepEmployeeNumber FROM customers WHERE customerNumber = '${customerID}';`;
+
+        return new Promise((resolve, reject)=>{
+            connection.query(customerQuery,(error, results)=>{
+                if(error) throw error;
+                resolve(results[0]);
+            });
+        });
+
+    }
+
+    function getEmployeeInfo(employeeID){
+        var employeeQuery = `SELECT firstName, lastName, officeCode, reportsTo FROM employees WHERE employeeNumber = '${employeeID}';`;
+
+        return new Promise((resolve, reject)=>{
+            connection.query(employeeQuery,(error, results)=>{
+                if(error) throw error;
+                resolve(results[0]);
+            });
+        });
+
+    }
+
+    function getOrderDetails(orderNumber){
+        var orderDetailsQuery = `SELECT productCode, (quantityOrdered*priceEach) AS totalPrice from orderdetails WHERE orderNumber = '${orderNumber}';`;
+
+        return new Promise((resolve, reject)=>{
+            connection.query(orderDetailsQuery,(error, results)=>{
+                if(error) throw error;
+                resolve(results);
+            });
+        });
+
+    }
+
+    function getProductName(productCode){
+        var productNameQuery = `SELECT productName FROM products WHERE productCode = '${productCode}';`;
+
+        return new Promise((resolve, reject)=>{
+            connection.query(productNameQuery,(error, results)=>{
+                if(error) throw error;
+                resolve(results);
+            });
+        });
+
+    }
 
 
 
-    // connection.query(productInfoQuery, function (error, results){
-    //     if(error) throw error;
-    //     // console.log(results[0]);
-    //
-    //     var productInfo = results[0];
-    //     var productCode = productInfo.productCode;
-    //
-    //     var orderQuery = `SELECT * FROM orderdetails WHERE productCode = '${productCode}';`;
-    //     connection.query(orderQuery, function (error, results) {
-    //        console.log(results)
-    //
-    //     });
-    //
-    //     res.render('product_info', { productInfo: productInfo });
-    //
-    // });
+    orderInfoPromise = getOrderInfo();
+    orderInfoPromise.then((data)=>{
+        orderDetails.customerNumber = data.customerNumber;
+        orderDetails.orderStatus = data.status;
+        return getCustomerInfo(orderDetails.customerNumber);
+    }).then((data)=>{
+        orderDetails.customerName = data.customerName;
+        orderDetails.customerCity = data.city;
+        orderDetails.employeeNumber = data.salesRepEmployeeNumber;
+        return getEmployeeInfo(orderDetails.employeeNumber);
+    }).then(function (data) {
+        orderDetails.employeeFirstName = data.firstName;
+        orderDetails.employeeLastName = data.lastName;
+        orderDetails.employeeOffice = data.officeCode;
+        orderDetails.employeeReportsTo = data.reportsTo;
+        return getOrderDetails(orderDetails.orderNumber);
+    }).then((data)=>{
+       //console.log(data);
+       for(let i = 0; i < data.length; i++){
+           orderDetails.productCodes.push(data[i].productCode);
+           orderDetails.totalPrices.push(data[i].totalPrice);
+       }
+    }).then((data)=>{
+        //console.log(orderDetails.productCodes);
+        productNames = [];
+        orderDetails.productCodes.map((productCode)=>{
+            productNames.push(getProductName(productCode));
+        });
 
+        Promise.all(productNames).then((data)=>{
+            for(let i = 0; i < data.length; i++){
 
+                orderDetails.productNames.push(data[i][0].productName)
+            }
+            console.log(orderDetails);
+            res.render('orders', {orderDetails: orderDetails});
+
+        });
+    });
 
 });
 
